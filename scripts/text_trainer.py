ac711d3a7a3319560fe79fbb99feeb15dbb1e133
Wang <teawang006@gmail.com>
2025-09-09 09:40:28 +0000

    reg 1.01

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -200,7 +200,7 @@ def main():
     )
     
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.01
     )
     
     args = parser.parse_args()
