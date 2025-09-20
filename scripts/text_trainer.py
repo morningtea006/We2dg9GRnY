369ca2ffcbeca90352d6f91fb79c19afdc1f926a
Wang <teawang006@gmail.com>
2025-09-20 03:43:26 +0000

    update

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -200,7 +200,7 @@ def main():
     )
     
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.001
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.02
     )
     
     args = parser.parse_args()
